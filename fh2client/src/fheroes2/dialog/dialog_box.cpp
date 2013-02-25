54c4eec7145447e1bb2771da5e352fd8c7960aef
Andrey Afletdinov <fheroes2@gmail.com>
2013-02-25 23:14:23 +0000

    update Dialog::RedrawableBox, Dialog::FrameBortder default width

--- fh2client/src/fheroes2/dialog/dialog_box.cpp
+++ fh2client/src/fheroes2/dialog/dialog_box.cpp
@@ -130,5 +130,5 @@ Dialog::RedrawableBox::RedrawableBox(u16 height, bool buttons)
 
 void Dialog::RedrawableBox::Redraw(void)
 {
-    BoxRedraw(GetArea().x, GetArea().y, count_middle);
+    BoxRedraw(GetRect().x, GetRect().y, count_middle);
 }
