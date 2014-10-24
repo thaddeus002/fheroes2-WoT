3262ca80edf5c09e24b45cf7740d853eda2aee8d
Andrey Afletdinov <fheroes2@gmail.com>
2014-10-24 06:58:23 +0000

    fix copyright

--- fheroes2/src/engine/serialize.cpp
+++ fheroes2/src/engine/serialize.cpp
@@ -1,12 +1,12 @@
 /***************************************************************************
- *   Copyright (C) 2014 by CataclysmNA team <cataclysm.newage@gmail.com>   *
+ *   Copyright (C) 2012 by Andrey Afletdinov <fheroes2@gmail.com>          *
  *                                                                         *
- *   Part of the Cataclysm: NewAge engine:                                 *
- *   https://launchpad.net/cataclysm.newage                                *
+ *   Part of the Free Heroes2 Engine:                                      *
+ *   http://sourceforge.net/projects/fheroes2                              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 3 of the License, or     *
+ *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
